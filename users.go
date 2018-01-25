bf4dc72038e05c408b7adf99aa0461020b867017
Benjamin Misell <benjamin@misell.cymru>
2018-01-25 19:27:52 +0000

    Fix bug in desplaying wrong user

--- users.go
+++ users.go
@@ -39,7 +39,7 @@ func handleUsersEdit(m *model.Model, page *Page, user *model.User, w http.Respon
 		return
 	}
 	if r.Method == "GET" {
-		displayWithContext(w, "users-edit", page, user, &UserContext{User: user})
+		displayWithContext(w, "users-edit", page, user, &UserContext{User: u})
 	} else if r.Method == "POST" {
 		r.ParseForm()
 		newUser := &model.User{
@@ -48,7 +48,7 @@ func handleUsersEdit(m *model.Model, page *Page, user *model.User, w http.Respon
 			Email: r.Form.Get("email"),
 			Phone: r.Form.Get("phone"),
 			IsAdmin: r.Form.Get("isadmin") == "checked",
-			Login: r.Form.Get("login"),
+			Login: r.Form.Get("logsin"),
 			Pass: r.Form.Get("pass"),
 			Disabled: u.Disabled,
 			TotpSecret: u.TotpSecret,
